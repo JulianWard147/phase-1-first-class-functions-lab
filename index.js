f8be051e46380260ab7a6e3bf1c45d167582605d
Julian Ward <wardjulianm@gmail.com>
2023-02-08 14:05:37 -0700

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+//const exampleDrivers = ['Sarah', 'Julian', 'Cyrus', 'Sophie']
+
+const returnFirstTwoDrivers = function(driverList){
+    return([driverList[0], driverList[1]])
+}
+
+//console.log(returnFirstTwoDrivers(exampleDrivers))
+
+const returnLastTwoDrivers = function(driverList){
+    return([driverList[driverList.length-2], driverList[driverList.length-1]])
+}
+
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers]
+
+const createFareMultiplier = function(multiplierNumber){
+    
+    return (fare) => fare*multiplierNumber
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(driverList, selectionFunction){
+    return(selectionFunction(driverList))
+}
